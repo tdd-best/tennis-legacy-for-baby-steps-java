e2f6c50a900519aa7c46dfedc90795c705a01dbd
Joey Chen <jay.hatelove@gmail.com>
2021-01-31 17:13:11 +0800

    refactor: extract fields and variables

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,52 +1,69 @@
 public class Tennis {
+    private final String all = "all";
+    private final String deuce = "deuce";
+    private final String firstPlayerName;
+    private final String secondPlayerName;
     private int secondPlayerScoreTimes;
     private int firstPlayerScoreTimes;
+    private String love;
+    private String fifteen;
+    private String thirty;
+    private String forty;
+
+    public Tennis() {
+        firstPlayerName = "Joey";
+        secondPlayerName = "Tom";
+    }
 
     public String score() {
+        love = "love";
+        fifteen = "fifteen";
+        thirty = "thirty";
+        forty = "forty";
         if (firstPlayerScoreTimes == 0 && secondPlayerScoreTimes == 0) {
-            return "love all";
+            return love + " " + all;
         }
         if (firstPlayerScoreTimes == 1 && secondPlayerScoreTimes == 0) {
-            return "fifteen love";
+            return fifteen + " " + love;
         }
         if (firstPlayerScoreTimes == 2 && secondPlayerScoreTimes == 0) {
-            return "thirty love";
+            return thirty + " " + love;
         }
         if (firstPlayerScoreTimes == 3 && secondPlayerScoreTimes == 0) {
-            return "forty love";
+            return forty + " " + love;
         }
         if (firstPlayerScoreTimes == 0 && secondPlayerScoreTimes == 1) {
-            return "love fifteen";
+            return love + " " + fifteen;
         }
         if (firstPlayerScoreTimes == 0 && secondPlayerScoreTimes == 2) {
-            return "love thirty";
+            return love + " " + thirty;
         }
         if (firstPlayerScoreTimes == 0 && secondPlayerScoreTimes == 3) {
-            return "love forty";
+            return love + " " + forty;
         }
         if (firstPlayerScoreTimes == 1 && secondPlayerScoreTimes == 1) {
-            return "fifteen all";
+            return fifteen + " " + all;
         }
         if (firstPlayerScoreTimes == 2 && secondPlayerScoreTimes == 2) {
-            return "thirty all";
+            return thirty + " " + all;
         }
         if (firstPlayerScoreTimes == 3 && secondPlayerScoreTimes == 3) {
-            return "deuce";
+            return deuce;
         }
         if (firstPlayerScoreTimes == 4 && secondPlayerScoreTimes == 4) {
-            return "deuce";
+            return deuce;
         }
         if (firstPlayerScoreTimes == 4 && secondPlayerScoreTimes == 3) {
-            return "Joey adv";
+            return firstPlayerName + " adv";
         }
         if (firstPlayerScoreTimes == 3 && secondPlayerScoreTimes == 4) {
-            return "Tom adv";
+            return secondPlayerName + " adv";
         }
         if (firstPlayerScoreTimes == 5 && secondPlayerScoreTimes == 3) {
-            return "Joey win";
+            return firstPlayerName + " win";
         }
         if (firstPlayerScoreTimes == 3 && secondPlayerScoreTimes == 5) {
-            return "Tom win";
+            return secondPlayerName + " win";
         }
         return null;
     }
